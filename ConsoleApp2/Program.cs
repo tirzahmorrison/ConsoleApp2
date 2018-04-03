bdb69eeaa87604e31b2c36360aaf3cc81522d0d3
Tirzah Morrison <tirzahmorrison@icloud.com>
2018-04-03 19:39:51 -0400

    number guessing game

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,26 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            string input = "";
+
+            while (true)
+            {
+                Console.WriteLine("Guess a random number between 1 and 100. Press 'Q' to quit.");
+                input = Console.ReadLine();
+                if (input.ToLower() == "q")
+                {
+                    break;
+                }
+                int actual = rnd.Next(1, 100);
+                if (Convert.ToInt32(input) == actual) {
+                    Console.WriteLine("correct");
+                } else {
+                    Console.WriteLine("You loose sucker. It was {0}", actual);
+                }
+            }
+            Console.WriteLine("Game Over");
+            Console.ReadLine();
         }
     }
 }
